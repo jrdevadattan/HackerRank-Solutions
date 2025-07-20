2a284bd6a724467b64cc02338aaa5708fb925f88
J R Deva Dattan <jrdevadattan2006@gmail.com>
2025-07-20 14:09:57 +0530

    Create Forming a Magic Sqaure.py

---
+++ Algorithms/Forming a Magic Sqaure.py
@@ -0,0 +1,26 @@
+def formingMagicSquare(s):
+    mag = [
+        [[8, 1, 6], [3, 5, 7], [4, 9, 2]],
+        [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
+        [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
+        [[2, 9, 4], [7, 5, 3], [6, 1, 8]],
+        [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
+        [[4, 3, 8], [9, 5, 1], [2, 7, 6]],
+        [[6, 7, 2], [1, 5, 9], [8, 3, 4]],
+        [[2, 7, 6], [9, 5, 1], [4, 3, 8]],
+    ]   
+    mi = float('inf')    
+    for m in mag:
+        c = 0
+        for i in range(3):
+            for j in range(3):
+                c += abs(s[i][j] - m[i][j])
+        mi = min(mi, c)
+    return mi
+
+if __name__ == '__main__':
+    s = []
+    for _ in range(3):
+        s.append(list(map(int, input().rstrip().split())))
+    result = formingMagicSquare(s)
+    print(result)
