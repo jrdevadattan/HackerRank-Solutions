93505414623881605cf533a136752cfecf73f9b3
J R Deva Dattan <jrdevadattan2006@gmail.com>
2025-07-30 16:10:19 +0530

    Create String Split and Join.py

---
+++ Python/String Split and Join.py
@@ -0,0 +1,9 @@
+def split_and_join(line):
+    l = line.strip().split()
+    s = '-'.join(l)
+    return s
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
