c3fe06d5075a700833c2ccb3e91e65596f254b4d
J R Deva Dattan <jrdevadattan2006@gmail.com>
2025-08-04 19:31:45 +0530

    Create Merge the Tools!.py

---
+++ Python/Merge the Tools!.py
@@ -0,0 +1,13 @@
+def merge_the_tools(string, k):
+    for i in range(0, len(string), k):
+        t = string[i:i+k]
+        u = ''
+        for c in t:
+            if c not in u:
+                u += c
+        print(u)
+
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
