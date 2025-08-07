f2d465d91f9f41fdd1f6b31ab4d3ba27b7847054
J R Deva Dattan <jrdevadattan2006@gmail.com>
2025-08-07 16:46:47 +0530

    Create Big Sorting.py

---
+++ Algorithms/Big Sorting.py
@@ -0,0 +1,12 @@
+def bigSorting(unsorted):
+    return sorted(unsorted, key=lambda x: (len(x), x))
+    
+if __name__ == '__main__':
+    n = int(input().strip())
+    unsorted = []
+    for _ in range(n):
+        unsorted_item = input()
+        unsorted.append(unsorted_item)
+    result = bigSorting(unsorted)
+    for i in result:
+        print(i)
